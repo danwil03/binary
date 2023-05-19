1a17305d7e1ed744584dc420ac69e48141793806
Daniel Wilk <wilkdaniel03.business@gmail.com>
2023-05-19 21:33:10 +0200

    Implementing printbin function using recursive approach with modulo operator

--- printbin/main.c
+++ printbin/main.c
@@ -9,8 +9,21 @@ void printbin_iterative(uint64_t n) {
     printf("\n");
 }
 
+void printbin_recursive_mod_helper(uint64_t n) {
+    if(n > 1) {
+        printbin_recursive_mod_helper(n / 2);
+    }
+    printf("%li", n % 2);
+}
+
+void printbin_recursive_mod(uint64_t n) {
+    printbin_recursive_mod_helper(n);
+    printf("\n");
+}
+
 int main() {
     printbin_iterative(123);
+    printbin_recursive_mod(123);
 
     return 0;
 }
